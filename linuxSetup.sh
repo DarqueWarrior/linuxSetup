8dd572865ad320a56d93275bcb20201210c8c328
Donovan Brown <dlbm3@hotmail.com>
2016-06-01 17:35:12 -0700

    Found easier way to install java on 16.04

--- linuxSetup.sh
+++ linuxSetup.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # Add repo for java
-sudo add-apt-repository -y ppa:webupd8team/java
+# sudo add-apt-repository -y ppa:webupd8team/java
 
 # Make sure you have the lastest packages
 sudo apt -y -q update
 
 # Now we can actually install Java.
-sudo apt -y install oracle-java8-installer
+# sudo apt -y install oracle-java8-installer
 
 # Now we need to set the default version of Java to use.
-sudo apt-get -y install oracle-java8-set-default
+# sudo apt-get -y install oracle-java8-set-default
+sudo apt -y install default-jdk
 
 # Install npm
 sudo apt -y install npm
@@ -25,4 +26,4 @@ sudo n stable
 sudo npm install -g bower gulp grunt
 
 # Install Maven
-sudo apt install maven
+sudo apt install -y maven
